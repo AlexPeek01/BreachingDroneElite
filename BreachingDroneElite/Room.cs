a6948a92fd8cc7a7fbc251c649ecd4aee0f4a762
Alex Peek <alex.peek@hotmail.com>
2019-12-12 10:57:34 +0100

    Created new repository
    Created new repository

---
+++ BreachingDroneElite/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Danillo Perez
+namespace BreachingDroneElite
+{
+    class Room
+    {
+        string RoomName = "Kamer 1";
+        int PersonCount = 5;
+        enum DangerLevel {Blue, Green, Yellow, Orange, Red};
+    }
+}
---
+++ BreachingDroneElite/Suspect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Alex Peek
+namespace BreachingDroneElite
+{
+    class Suspect
+    {
+        private string _Name;
+        private string _Weapon;
+        private string _Utility;
+        enum _Priority {Alpha, Bravo, Charlie, Delta};
+
+        public Suspect(string Name, string Weapons, string Utility, string Priority)
+        {
+            this._Priority = Priority
+        }
+    }
+}
