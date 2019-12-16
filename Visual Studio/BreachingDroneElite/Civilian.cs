f4f74d662a346aad41c29963270e6d3b11a93ac5
Alex Peek <alex.peek@hotmail.com>
2019-12-16 14:17:40 +0100

    Leerdoelen aangetoont

--- Visual Studio/BreachingDroneElite/Civilian.cs
+++ Visual Studio/BreachingDroneElite/Civilian.cs
@@ -3,13 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Alex Peek
+//Danillo Perez
 namespace BreachingDroneElite
 {
     class Civilian
     {
-        string Gender;
-        int age;
-        List<string> Injuries = new List<string>();
-    }
+        private string _Gender;
+        private int _Age;
+        private string _Injuries;
+        private string _Additional_Info;
+
+        public Civilian (string Gender, int Age, string Injuries, string Info)
+            {
+            this._Gender = Gender;
+            this._Age = Age;
+            this._Injuries = Injuries;
+            this._Additional_Info = Info;
+            }
+        public string Gender
+        {
+            get
+            {
+                return _Gender;
+            }
+            set
+            {
+                _Gender = value;
+            }
+        }
+            public int Age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                _Age = value;
+            }
+        }
+            public string Injuries
+        {
+            get
+            {
+                return _Injuries;
+            }
+            set
+            {
+                _Injuries = value;
+            }
+        }
+        public string Info 
+        {
+            get
+            {
+                return _Additional_Info;
+            }
+            set
+            {
+                _Additional_Info = value;
+            }
+        }
+    }        
 }
