8f6f0464da3627a2cb004cf4d31eb7c0bc5737fa
Actionhank1990 <56202503+Actionhank1990@users.noreply.github.com>
2020-01-15 11:17:54 +0100

    Added Login Added Encryption functionality

---
+++ Visual Studio/BreachingDroneElite/LoginWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BreachingDroneElite
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginService.EncryptPassword(Password.Text) == LoginService.hashedPassword)
+            {
+                LoginService.WindowOpenClose();
+                this.Close();
+            }
+            else
+            {
+                IncorrectPass.Content = "Incorrect Password";
+                
+            }
+        }
+    }
+}
